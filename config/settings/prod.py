8f3ef2bd8200ac06ef520aa4ed68f5fa28570f45
박상희 <parksanghee223@gmail.com>
2021-08-23 09:02:29 +0900

    domain add

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.38.66.8', 'pybo.kr']
+ALLOWED_HOSTS = ['3.38.66.8', 'pybo412.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
