06eebdd954a4787d8684025a844a006d8ec781aa
박상희 <parksanghee223@gmail.com>
2021-08-23 15:59:23 +0900

    new db

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,7 +9,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pybo',
         'USER': 'dbmasteruser',
-        'PASSWORD': '=`z<*ffM?~M9q^NH&>2Y2nDqb^241dk%|',
+        'PASSWORD': '`z<*ffM?~M9q^NH&>2Y2nDqb^241dk%|',
         'HOST': 'ls-4e2a340f0b45f7a149659c882333019cbaf6f259.c73y3p1tyvfn.ap-northeast-2.rds.amazonaws.com',
         'PORT': '5432',
     }
