ffe94ca040f1f78dd259cc27592a3b607bfbc249
박상희 <parksanghee223@gmail.com>
2021-08-25 23:18:46 +0900

    수정내용 코멘트

--- config/wsgi.py
+++ config/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+for more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
